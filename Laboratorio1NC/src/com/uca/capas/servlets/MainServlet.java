793e73ec03b46e1a075d0d3ef4c98ae2e03bcee6
RoxanaCortez <00502211@uca.edu.sv>
2020-03-27 14:47:39 -0600

    Tarea finalizada

---
+++ Laboratorio1NC/src/com/uca/capas/servlets/MainServlet.java
@@ -0,0 +1,57 @@
+package com.uca.capas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String usuario = request.getParameter("usuario");
+		String contra = request.getParameter("contra");
+		
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		if(usuario.equals("admin") && contra.equals("admin")) {
+			out.println("<h3>" + "ACCESO CORRECTO" + "</h3>");
+		}else {
+			out.println("<h3>" + "ACCESO INCORRECTO" + "</h3>");
+		}
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
